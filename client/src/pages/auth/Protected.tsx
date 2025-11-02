e8877e29dd729e9c6b40142ec3edb709edabb3b9
rahul <rg4005450@gmail.com>
2025-11-02 19:24:57 +0530

    UI fixes

--- client/src/pages/auth/Protected.tsx
+++ client/src/pages/auth/Protected.tsx
@@ -5,8 +5,8 @@ import { Loader2 } from "lucide-react";
 export default function ProtectedRoute() {
   const { isAuthenticated, loading } = useAuth();
 
-  if (loading) return <div className="flex justify-center items-center">
-      <Loader2 className="w-15 h-15"/>;
+  if (loading) return <div className="flex justify-center items-center h-[85vh]">
+      <Loader2 className="w-15 h-15 animate-spin" color="white"/>;
     </div>
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
